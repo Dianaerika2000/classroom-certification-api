1a08cfdbd0106396159e7b06a081e5857d261268
Herbas Fernandez Alejandra Nicole <herbasfernandez.alejandra@gmail.com>
2024-09-28 17:10:22 -0400

    build: Add Swagger for documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
