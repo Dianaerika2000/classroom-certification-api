ab68fc2395b4cc351e50078919a7e29691160942
Herbas Fernandez Alejandra Nicole <herbasfernandez.alejandra@gmail.com>
2024-09-28 19:19:15 -0400

    build: Add cors

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+  })
+
   await app.listen(3000);
 }
 bootstrap();
