dad9316524922c3ebf5d797b679bf4215b112b0d
Montaño Flores Diana Erika <dianaerika.montano16@gmail.com>
2025-01-04 22:06:55 -0400

    refactor(summary): remove unnecessary getSummaryByForm function from controller and service

--- src/summary/summary.controller.ts
+++ src/summary/summary.controller.ts
@@ -6,11 +6,11 @@ import { ValidRoles } from '../auth/enums/valid-roles';
 
 @ApiTags('Summary')
 @Controller('summary')
-@Auth(ValidRoles.admin, ValidRoles.evaluator)
 export class SummaryController {
   constructor(private readonly summaryService: SummaryService) {}
 
   @Post('calculate/form/:formId')
+  @Auth(ValidRoles.admin, ValidRoles.evaluator)
   @ApiResponse({ status: 201, description: 'Summary calculated and saved successfully' })
   @ApiResponse({ status: 400, description: 'Bad Request' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
@@ -19,21 +19,6 @@ export class SummaryController {
 
     return {
       message: "Resumen calculado exitosamente",
-      data: {
-        summary: summaryData,
-      },
-    };
-  }
-
-  @Get('form/:formId')
-  @ApiResponse({ status: 200, description: 'Summary retrieved successfully' })
-  @ApiResponse({ status: 401, description: 'Unauthorized' })
-  @ApiResponse({ status: 404, description: 'Summary not found for the provided form' })
-  async getSummaryByForm(@Param('formId') formId: number) {
-    const summaryData = await this.summaryService.getSummaryByForm(formId);
-
-    return {
-      message: "Resumen obtenido exitosamente",
       data: {
         summary: summaryData,
       },
